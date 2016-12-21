fb23669642d818dd2355135ad5d4f3818283130e
Artem Bykov <abykov@e1r8p14.unit.ua>
2016-12-21 16:15:20 +0200

    Working beta ver

--- Makefile
+++ Makefile
@@ -1,19 +1,25 @@
 NAME = fillit
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
-SRC = *.c
+SRC = 	fl_add_tetra.c		fl_get_height.c		fl_solve.c	\
+		fl_free_slot.c		fl_isvalid.c		main.c		\
+		fl_generate_map.c	fl_lstfunc.c
 OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
-	
+	@echo "Done"
+
 $(NAME): $(OBJ)
-	ar rc libft.a $(OBJ)
+	$(CC) $(OBJ) libft.a
+
 .o:
 	$(CC) -c $(SRC) $(CFLAGS)
+
 clean:
 	rm -rf $(OBJ)
+
 fclean: clean
-	rm -rf libft.a
+	rm -rf fillit
 
 re: fclean all
-	@echo "Done"
+	@echo "ReDone"
--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include "fillit.h"
 #include "libft.h"
 
-int			isvalid(char const *path)
+int			isvalid(char const *path, int fd)
 {
 	char	buf[22];
 	int		len;
-	int		fd;
+	char	last;
 
 	ft_bzero(buf, 22);
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
+	if ((fd = open(path, O_RDONLY)) < 0)
 	{
 		ft_putendl_fd("file does not exist", 2);
 		exit(1);
@@ -22,28 +21,28 @@ int			isvalid(char const *path)
 			ft_putendl_fd("Invalid input", 2);
 			exit(1);
 		}
+		last = buf[20];
 		ft_bzero(buf, 21);
 		len = read(fd, buf, 21);
 	}
+	if (!last)
+		return (1);
+	return (0);
 }
 
-void		fl_read(char const *path, t_lst **head, char letter)
+void		fl_read(char const *path, t_lst **head)
 {
 	char	buf[22];
 	int		len;
 	int		fd;
+	char	letter;
 
 	ft_bzero(buf, 22);
 	fd = open(path, O_RDONLY);
-
+	letter = 'A';
 	len = read(fd, buf, 21);
 	while (len)
 	{
-		if (len < 20 || !fl_isvalid(buf))
-		{
-			ft_putendl_fd("Invalid input", 2);
-			exit(1);
-		}
 		fl_add_tetra(head, buf, letter++);
 		ft_bzero(buf, 21);
 		len = read(fd, buf, 21);
@@ -56,7 +55,7 @@ int			main(int argc, char **argv)
 	t_lst	*head;
 	char	*map;
 	int		n;
-	char	letter;
+	int		fd;
 
 	if (argc != 2)
 	{
@@ -66,12 +65,15 @@ int			main(int argc, char **argv)
 	n = 2;
 	map = fl_getmap(n);
 	head = 0;
-	letter = 'A';
-	fl_read(argv[1], &head, letter);
-	while (!fl_solve(map, n, head))
+	if (isvalid(argv[1], fd = -1))
+		fl_read(argv[1], &head);
+	else
 	{
-		fl_realloc(&map, ++n);
+		ft_putendl_fd("Input error: Invalid last character", 2);
+		return (1);
 	}
+	while (!fl_solve(map, n, head))
+		fl_realloc(&map, ++n);
 	printf("%s\n", map);
 	return (0);
 }
